be4589bbd1590e7abfea248e1881267b3cb25813
shihaab <roushi02@gmail.com>
2017-06-13 11:32:35 +0200

    operator update(ash)
    we have added a new op: Ash

---
+++ operators/ash.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+<title>Ash</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="keywords" content="R6S, Rainbow Six Siege, wiki, history, rainbow, six, siege, maps, operators, loadout, tombstone, squad, tombstone-squad, tombstone squad, shihaab, ivo, ivo kruijssen, shihaab rouine">
+<meta name="description" content="Learn all about Rainbow Six Siege! Rainbow Six Siege Operators, Maps And The History Of Siege!">
+<meta name="author" content="Shihaab Rouine">
+<link rel="apple-touch-icon" sizes="180x180" href="../apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="192x192" href="../android-chrome-192x192.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png">
+<link rel="manifest" href="../manifest.json">
+<link rel="mask-icon" href="../safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#b91d47">
+<meta name="msapplication-TileImage" content="../mstile-144x144.png">
+<meta name="theme-color" content="#ffffff">
+<link rel="stylesheet" href="../public/css/w3.css">
+<link rel="stylesheet" href="../public/aos-master/dist/aos.css">
+<link rel="stylesheet" href="../public/css/font-awesome-animation.min.css">
+<link rel="stylesheet" href="../public/css/bootstrap.min.css" type="text/css">
+<link href="https://file.myfontastic.com/m2Bio7sPG2WcJ4jcJb7xzU/icons.css" rel="stylesheet">
+<script src="../public/aos-master/dist/aos.js"></script>
+<script src="../public/js/jquery.js"></script>
+<script src="../public/js/bootstrap.js"></script>
+<script src="https://use.fontawesome.com/c06b312836.js"></script>
+<link rel="stylesheet" href="../public/css/main.css" type="text/css">
+<body oncopy="return false" oncut="return false" onpaste="return false">
+
+<!-- Navbar (sit on top) -->
+<div class="w3-top">
+  <div class="w3-bar w3-white w3-wide w3-padding w3-card-2">
+    <a href="#home" class="w3-bar-item w3-button"><span class="txt-color anim-text-flow">Rainbow Six Siege</span></a>
+    <!-- Float links to the right. Hide them on small screens -->
+    <div class="w3-right w3-hide-small">
+      <a href="#bio" class="w3-bar-item w3-button"><span class="txt-color anim-text-flow">Bio</span></a>
+      <a href="#overview" class="w3-bar-item w3-button"><span class="txt-color anim-text-flow">Overview</span></a>
+      <a href="#loadout" class="w3-bar-item w3-button"><span class="txt-color anim-text-flow">Loadout</span></a>
+    </div>
+  </div>
+</div>
+
+<!-- Header -->
+<span id="up"></span>
+<header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
+  
+  <video src="media/ash2.mp4" muted autoplay loop alt="Ash" width="1500" height="800">HTML5 Video is required for this example</video> 
+  <div class="w3-display-middle w3-margin-top w3-center">
+    <h1 class="w3-xxlarge w3-text-white"><span class="w3-padding w3-black w3-opacity-min"><b class="siege-text-1">FBI SWAT</b></span> <span class="w3-hide-small w3-text-light-grey siege-text-2" data-content="Siege">Ash</span></h1>
+  </div>
+</header>
+
+<!-- Page content -->
+<div class="w3-content w3-padding" style="max-width:1564px">
+
+  <!-- Project Section -->
+  <div class="w3-container w3-padding-32" id="bio">
+  	<div class="text-center">
+  		<h3 class="w3-padding-16 w3-jumbo" data-aos="flip-up">BIO</h3>
+  	</div>
+    <div class="container r6-text">
+    	<p>Born and raised in Jerusalem, Israel, Cohen enrolled at Tel Aviv University after secondary school in the Engineering program. It was during this time she did a semester abroad in Boston University. After college, she enlisted in the Israeli Defense Forces. Her engineering background made her a great candidate for mechanical maintenance and demolitions.</p>
+    	<p>She was later transferred to FBI SWAT due to an international law enforcement exchange program. Cohen brought her knowledge of demolitions with her in producing a weapon-mounted breach charge capable of blowing open a door or wall from a distance.</p>
+    	<h3>Psychological Profile</h3>
+    	<p>Her military training as a demolitions specialist has left Cohen with an inflated sense of self. She can be aggressive, impulsive, and overly confident. She is quick to action, which can be an asset or fatal for someone in her line of work.</p>
+	</div>
+  </div>
+
+  
+  <!-- About Section -->
+  <div class="w3-container w3-padding-32" id="overview">
+    <div class="w3-center" data-aos="fade-left" data-aos-anchor="#operators-anchor" data-aos-offset="500" data-aos-duration="500"><i class="operators-header"><h3 class="w3-border-bottom w3-border-light-grey w3-padding-16 w3-tag w3-jumbo">Overview</h3></i></div>
+    <div class="container overview-list">
+		<p>Ash can fire two remote breaching charges to surfaces that would normally be hard or impossible to reach, like higher windows or even ceilings; enemies rarely expect the floor beneath them to blow up so use this to surprise them.</p>
+		<p>Remote breaches will detonate automatically once they get stuck on a wall, meaning that they cannot be affected by a Signal Disruptor, making them the best way to breach against a team with Mute in their ranks.</p>
+		<p>The Remote Breach Launcher is also very useful to destroy a wall from a safe distance if you expect enemy fire. Keep in mind that it is not a rocket launcher and will do reduced damage to players.</p>
+		<p>You can try using the Remote Breach to blow up a barricaded window from below and let a teammate throw a frag grenade. You'll probably deal a lot of damage to the enemy while remaining safe from enemy fire.</p>
+		<p>Getting struck directly by a Breaching Round will result in dealing 50 damage, regardless of armor, though the charge will not explode.</p>
+		<p>Certain walls can't be completely covered by Reinforcements, where the gap is enough for Ash to shoot through once the uncovered part of the wall is destroyed by her Breach Launcher.</p>
+		<p>Ash has a smaller headshot hitbox than some other operators making it easier to peek around corners safely.</p>
+		<p>The remote breach charge can also destroy deployable shields and can injure the Operator behind it.</p>
+    </div>
+  </div>
+	<a href="#up"><div class="item item--secondary animated-hover faa-parent" data-aos="fade-right" data-aos-anchor="#trigger-left" data-aos-anchor-placement="top-top"><button class="w3-button faa-bounce"><i class="fa fa-angle-double-up" aria-hidden="true"></i></button>
+	<span>
+		
+	</span>
+		</div></a>
+
+	
+  <div class="w3-row-padding operators">
+    
+  </div>
+
+
+  <!-- Loadout Section -->
+  <div class="w3-container w3-padding-32" id="loadout">
+	  	<h3 class="w3-padding-16 w3-center">Loadout</h3>
+		<div class="container">
+			<div class="rainbow r6-text">
+				<h2>Lorem ipsum</h2>
+				<p>Loadout of operatorLoadout of operatorLoadout of operatorLoadout of operatorLoadout of operatorLoadout of operatorLoadout of operatorLoadout of operator</p>
+			</div>
+			<div class="history-panel w3-panel w3-black w3-text-white" data-aos="fade-left">
+				<i class="fa fa-quote-right w3-xxlarge w3-margin-right"></i><p>"Breach Charge ready!"</p>
+			</div>
+			<div class="development r6-text">
+				<h2>Loadout Description</h2>
+				<p>The M120 CREM was developed as a portable projectile breaching weapon. Similar to other explosive breaching tools used by American and Israeli armed forces, the M120 CREM is capable of breaching through doors without the need to focus on destroying hinges or locks for entry. Unlike other explosive breaching methods, the M120 CREM is comparatively portable with its decreased size and weight, allowing it to be carried as a secondary weapon. The M120 CREM has been successfully utilized by FBI SWAT in penetrating fortified compounds during hostage and search and rescue operations.</p>
+			</div>
+			<div class="history-panel w3-panel w3-black w3-text-white" data-aos="fade-left">
+				<i class="fa fa-quote-right w3-xxlarge w3-margin-right"></i><p>According to the in-game biography of Mira, Ash developed a rivalry with her following Operation Blue Orion.</p>
+			</div>
+		</div>
+  	</div>
+  
+<!-- End page content -->
+</div>
+
+<!-- Footer -->
+<footer class="w3-center w3-padding-16 powered-footer">
+  <p>Powered by <br><a href="" title="?" target="_blank" class="powered-by">Ivo Kruijssen</a> &#9885; <a  href="https://shihaab.myportfolio.com/" title="My Portfolio" target="_blank" class="powered-by"> Shihaab Rouine</a> <br><a href="http://realfavicongenerator.net/featured_favicons/p1bi3mm8ebndq40dts0d7p1lrt6" target="_blank"><span class="txt-color anim-text-flow">Tombstone SQD &trade;</span></a></p>
+</footer>
+
+<script>
+AOS.init({
+      disable: 'mobile'
+    });
+	AOS.init({
+	  duration: 1200,
+	  easing: 'ease-in-out-back'
+	});
+</script>
+<!-- mooth scroll -->
+<script>
+	$('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(t){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var e=$(this.hash);(e=e.length?e:$("[name="+this.hash.slice(1)+"]")).length&&(t.preventDefault(),$("html, body").animate({scrollTop:e.offset().top},1e3,function(){var t=$(e);if(t.focus(),t.is(":focus"))return!1;t.attr("tabindex","-1"),t.focus()}))}});
+</script>
+<script>
+	$(".txt-color").html(function(n,t){return"<span>"+$.trim(t).split("").join("</span><span>")+"</span>"});
+</script>
+
+
+</body>
+</html>
